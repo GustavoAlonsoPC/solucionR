d29a84b2313a437358d2760bf3badb5af1a739a0
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-04-17 15:28:16 -0500

    add routes to home and categories from dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,33 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './screens/home/home.component';
 import { CategoryComponent } from './screens/category/category.component';
+import { DashboardComponent } from './screens/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path:'', redirectTo:'home', pathMatch:'full' },
-  { path:'home', component:HomeComponent},
-  { path:'categories', component:CategoryComponent}
+  { 
+    path:'', 
+    redirectTo:'dashboard', 
+    pathMatch:'full' 
+  },
+  { 
+    path:'dashboard', 
+    component: DashboardComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'categories',
+        component: CategoryComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
