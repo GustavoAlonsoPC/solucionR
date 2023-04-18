8215b7c4f2ba55d81c11f6271de32abd4295c590
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-04-18 15:02:08 -0500

    add loading products message

--- src/app/screens/category/category.component.ts
+++ src/app/screens/category/category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/models/products.model';
+import { ServiceMainService } from 'src/app/shared/services/service-main.service';
 
 @Component({
   selector: 'app-category',
@@ -8,21 +9,27 @@ import { Product } from 'src/app/shared/models/products.model';
 })
 
 export class CategoryComponent implements OnInit {
-  public bodyArray:Array<Product>=[
-    {
-        "id": 3,
-        "title": "Tennis adidas",
-        "price": 250000.0,
-        "image": ""
-    }
-  ] 
-  constructor() { }
+  
+  public bodyArray:Array<Product>=[];
+
+  loading: boolean = true;
+
+  constructor(private service: ServiceMainService) { }
 
   ngOnInit(): void {
+
+    this.loadProducts();
   }
 
   addItem(item: any){
     console.log(item)
+  }
+
+  loadProducts(): void {
+    setTimeout(() => {
+      this.service.getProducts().subscribe(res => this.bodyArray = res);
+      this.loading = false;
+    }, 2000);
   }
 
 }
--- src/app/shared/services/service-main.service.ts
+++ src/app/shared/services/service-main.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment'
+import { Observable } from 'rxjs';
+import { Product } from '../models/products.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +13,7 @@ export class ServiceMainService {
 
   constructor(public http:HttpClient) { }
 
-  
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.url);
+  }
 }
