2776867273c50e8c1ecdf746930b1badbd441faa
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-04-20 09:28:08 -0500

    listen to last item added

--- src/app/screens/car-products/car-products.component.ts
+++ src/app/screens/car-products/car-products.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, takeLast } from 'rxjs';
 import { Product } from 'src/app/shared/models/products.model';
 import { ProductState } from '../ngrx/product.state';
 import { Store, select } from '@ngrx/store';
@@ -13,14 +13,21 @@ import { __values } from 'tslib';
 export class CarProductsComponent implements OnInit {
 
   products$: Observable<Product[]>;
-  prods: Array<Product> = [];
+
+  lastItem: Product | undefined = {id: 0, title: 'No item added', image: '', price: 0};
 
   constructor(private store: Store<{cart: ProductState}>) {
     this.products$ = store.select(state => state.cart.products);
+    this.products$.subscribe(val => {
+      if(typeof val !== 'undefined') {
+        this.lastItem = val.at(-1)
+      };
+    });
    }
    
 
   ngOnInit(): void {
+    
   }
 
 }
